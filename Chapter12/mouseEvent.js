e9068f3b01b5a4542500114fefc5d2abef5ee300
niuxiaopeng <648279073@qq.com>
2019-01-23 23:46:29 +0900

    2019年1月23日23:46:12第12章结束

--- Chapter12/mouseEvent.js
+++ Chapter12/mouseEvent.js
@@ -1,3 +1,13 @@
+function onMouseInOutEvent(event){
+    let msgList=document.getElementById("msgList");
+    let targetId=event.target.id;
+
+    let type=event.type;
+    let item=document.createElement("li");
+    item.textContent+=targetId+"に"+type+"が発生";
+    msgList.appendChild(item);
+}
+
 function init(){
     let outerBox=document.getElementById("outerBox");
     outerBox.addEventListener("mousemove",function(event){
@@ -5,6 +15,30 @@ function init(){
 
         innerBox.textContent="x="+event.offsetX+":y="+event.offsetY;
     });
+
+    outerBox.addEventListener("mouseenter",onMouseInOutEvent);
+    outerBox.addEventListener("mouseleave",onMouseInOutEvent);
+    outerBox.addEventListener("mouseover",onMouseInOutEvent);
+    outerBox.addEventListener("mouseout",onMouseInOutEvent);
+}
+
+function removeMouseInOutEvent(){
+    let outerBox=document.getElementById("outerBox");
+
+    outerBox.removeEventListener("mouseenter",onMouseInOutEvent);
+    outerBox.removeEventListener("mouseleave",onMouseInOutEvent);
+    outerBox.removeEventListener("mouseover",onMouseInOutEvent);
+    outerBox.removeEventListener("mouseout",onMouseInOutEvent);
+}
+
+function addMouseInOutEvent(){
+    let outerBox=document.getElementById("outerBox");
+    outerBox.addEventListener("mouseenter",onMouseInOutEvent);
+    outerBox.addEventListener("mouseleave",onMouseInOutEvent);
+    outerBox.addEventListener("mouseover",onMouseInOutEvent);
+    outerBox.addEventListener("mouseout",onMouseInOutEvent);
 }
 
 window.addEventListener("DOMContentLoaded",init);
+
+
